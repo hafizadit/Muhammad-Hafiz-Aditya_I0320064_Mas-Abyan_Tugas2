ad84781520691afc5dd273a147eee0734b137e77
hafizadit <mha.hafiz21@gmail.com>
2021-03-08 17:00:22 +0700

    Menambahkan soal1.py

---
+++ soal1.py
@@ -0,0 +1,10 @@
+# Program Menghitung Luas Persegi Panjang
+
+print("\nProgram Menghitung Luas Persegi Panjang\n================================")
+
+p = float(input("Masukkan nilai panjang :"))
+l = float(input("Masukkan nilai lebar :"))
+
+L = p * l
+
+print("\nPersegi panjang anda memiliki panjang", p, "dan memiliki lebar",l,"\nSehingga luas persegi panjang anda =", L)

485785d1111803d92376609c7913de9a754db867
hafizadit <mha.hafiz21@gmail.com>
2021-03-08 21:33:57 +0700

    Menyelesaikan Soal nomor 1

--- soal1.py
+++ soal1.py
@@ -1,3 +1,4 @@
+
 # Program Menghitung Luas Persegi Panjang
 
 print("\nProgram Menghitung Luas Persegi Panjang\n================================")
@@ -5,6 +6,49 @@ print("\nProgram Menghitung Luas Persegi Panjang\n==============================
 p = float(input("Masukkan nilai panjang :"))
 l = float(input("Masukkan nilai lebar :"))
 
-L = p * l
+Lpp = p * l
 
-print("\nPersegi panjang anda memiliki panjang", p, "dan memiliki lebar",l,"\nSehingga luas persegi panjang anda =", L)
+print("\nPersegi panjang anda memiliki panjang =", p, "dan memiliki lebar =",l,"\nSehingga luas persegi panjang anda =", Lpp)
+
+
+# Program menghitung luas lingkaran
+
+print("\nProgram Menghitung Luas Lingkaran\n================================")
+
+r = float(input("Masukkan nilai jari-jari :"))
+
+Ll = (3.14) * r ** 2
+
+print("\nLingkaran anda memiliki jari-jari =", r, "\nSehingga luas lingkaran anda =", Ll)
+
+
+# Program Luas Kubus
+
+print("\nProgram Menghitung Luas Kubus\n================================")
+
+s = float(input("Masukkan nilai rusuk kubus :"))
+
+Lk = 6 * s ** 2
+
+print("\nKubus anda memiliki rusuk =", s, "\nSehingga luas kubus anda =", Lk)
+
+
+# Program Konversi suhu celcius ke farenheit
+
+print("\nProgram Konversi Suhu Celcius ke Farenheit\n================================")
+
+c = float(input("Masukkan suhu dalam Celcius :"))
+
+f = (9/5 * c) + 32
+
+print("\nSuhu dalam celcius =", c, "derajat celcius\nSehingga suhu dalam farenheit =", f,"derajat farenheit")
+
+# Program Konversi suhu Reamur ke Kelvin
+
+print("\nProgram Konversi Suhu Reamur ke Kelvin\n================================")
+
+reamur = float(input("Masukkan suhu dalam Reamur :"))
+
+kelvin = (5/4 * reamur) + 273
+
+print("\nSuhu dalam reamur =", reamur, "derajat reamur\nSehingga suhu dalam Kelvin =", kelvin,"derajat kelvin")

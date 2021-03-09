b94be2d582681dc51259fcc3a18504a341dee782
hafizadit <mha.hafiz21@gmail.com>
2021-03-09 19:07:32 +0700

    menambah exercise 2_1

---
+++ exercise2_1.py
@@ -0,0 +1,29 @@
+#Tipe data boolean
+x = True
+print(x)
+print(type(x))
+
+#Tipe data string
+x = "Aku suka python"
+print(x)
+print(type(x))
+
+#Tipe data integer
+x = 69
+print(x)
+print(type(x))
+
+#Tipe data float
+x = 3.22
+print(x)
+print(type(x))
+
+#Tipe data hexadecimal
+x = hex(20)
+print(x)
+print(type(x))
+
+#Tipe data complex
+x = (20j)
+print(x)
+print(type(x))
